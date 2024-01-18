3f4c5b3fd620d607c387dc3c02329825cbcac5b6
Vinicius Dias <vi_aleixodias@hotmail.com>
2024-01-18 13:39:40 -0300

    feat: added go test cover

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -2,12 +2,26 @@ package arraysslices
 
 import "testing"
 
+// go test -cover
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
+	// t.Run("coleção de 5 numeros", func(t *testing.T) {
+	// 	numbers := []int{1, 2, 3, 4, 5}
+	// 	got := Sum(numbers)
+	// 	want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+	// 	if got != want {
+	// 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	// 	}
+	// })
+
+	t.Run("coleção de qualquer tamanho", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		resultado := Sum(numbers)
+		esperado := 6
+
+		if resultado != esperado {
+			t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numbers)
+		}
+	})
 }
